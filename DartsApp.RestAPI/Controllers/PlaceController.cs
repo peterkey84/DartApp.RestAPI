af28c2749c3ec8997c431249f0629b68e4a50a4e
Piotr Kluczny <piotrkluczny@europarts.pl>
2024-07-29 22:28:25 +0200

    Refactoring PlaceController

--- DartsApp.RestAPI/Controllers/PlaceController.cs
+++ DartsApp.RestAPI/Controllers/PlaceController.cs
@@ -21,48 +21,40 @@ namespace DartsApp.RestAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllPlaces()
+        public async Task<IEnumerable<PlaceCreateDto>> GetAllPlaces()
         {
-            var places = await _placeService.GetAllAsync();
+            IEnumerable<PlaceCreateDto> places = await _placeService.GetAllAsync();
 
-            return Ok(places);
+            return places;
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetPlaceById(int id)
+        public async Task<PlaceCreateDto> GetPlaceById(int id)
         {
-
             var place = await _placeService.GetByIdAsync(id);
 
-            if(place == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(place);
+            return place;
         }
 
         
         [HttpPost]
-        public async Task<IActionResult> AddNewPlace(PlaceCreateDto placeDto)
+        public async Task AddNewPlace(PlaceCreateDto placeDto)
         {
             await _placeService.AddAsync(placeDto);
 
-            return CreatedAtAction(nameof(GetPlaceById), new { id = placeDto.Id }, placeDto);
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdatePlaceById(int id, PlaceCreateDto placeDto)
+        public async Task UpdatePlaceById(int id, PlaceCreateDto placeDto)
         {
             await _placeService.UpdateAsync(id, placeDto);
-            return Ok();
         }
 
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePlaceById(int id)
+        public async Task DeletePlaceById(int id)
         {
             await _placeService.DeleteAsync(id);
-            return Ok();
+
         }
     }
 }
