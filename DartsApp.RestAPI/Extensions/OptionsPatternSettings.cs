02135db7df0ac222bd6430095f1d73c6080dd240
Piotr Kluczny <piotrkluczny@europarts.pl>
2024-08-20 23:16:06 +0200

    Added extension related to options patters settings

---
+++ DartsApp.RestAPI/Extensions/OptionsPatternSettings.cs
@@ -0,0 +1,16 @@
+﻿using DartsApp.RestAPI.Settings;
+
+namespace DartsApp.RestAPI.Extensions
+{
+    public static class OptionsPatternSettings
+    {
+
+        public static void AddOptionsPatternSettings(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<BoardSettings>(configuration.GetSection(nameof(BoardSettings)));
+            services.Configure<BoardLimits>(configuration.GetSection(nameof(BoardLimits)));
+            services.Configure<PlayerSettings>(configuration.GetSection(nameof(PlayerSettings)));
+            services.Configure<PlayerValidation>(configuration.GetSection(nameof(PlayerValidation)));
+        }
+    }
+}
